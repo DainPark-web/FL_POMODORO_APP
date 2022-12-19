cd23aefdd38f5d7ad7e1ccb0d9986d6bfebffd24
dain <57316771+DainPark-web@users.noreply.github.com>
2022-12-19 12:57:18 +0100

    update

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,9 +10,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  void onPressedd() {}
   int totalSeconds = 1500;
-
+  bool isRunning = false;
   late Timer timer;
 
   void onTick(Timer timer) {
@@ -26,6 +25,16 @@ class _HomeScreenState extends State<HomeScreen> {
       const Duration(seconds: 1),
       onTick,
     );
+    setState(() {
+      isRunning = true;
+    });
+  }
+
+  void onPausePreesed() {
+    timer.cancel();
+    setState(() {
+      isRunning = false;
+    });
   }
 
   @override
@@ -52,8 +61,10 @@ class _HomeScreenState extends State<HomeScreen> {
             flex: 2,
             child: Center(
               child: IconButton(
-                icon: const Icon(Icons.play_circle_outline),
-                onPressed: onStartPressed,
+                icon: Icon(isRunning
+                    ? Icons.pause_circle_outline
+                    : Icons.play_circle_outline),
+                onPressed: isRunning ? onPausePreesed : onStartPressed,
                 iconSize: 89,
                 color: Theme.of(context).cardColor,
               ),
